ba0473cf1a8e4e9d5fc1d103748ceb4fa8bda56a
Alisson Evangelista <108416106+AlissonED@users.noreply.github.com>
2025-03-15 11:27:19 -0300

    Add files via upload

---
+++ Aula4Media/Aula4Media/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Aula4Media
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.trabalho = new System.Windows.Forms.TextBox();
+            this.n2 = new System.Windows.Forms.TextBox();
+            this.n1 = new System.Windows.Forms.TextBox();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.btcalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(283, 23);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(119, 25);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Nota de P1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(283, 162);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(119, 25);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Nota de P2";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(283, 303);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(178, 25);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "Nota de Trabalho";
+            this.lbl3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // trabalho
+            // 
+            this.trabalho.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trabalho.Location = new System.Drawing.Point(288, 364);
+            this.trabalho.Name = "trabalho";
+            this.trabalho.Size = new System.Drawing.Size(396, 26);
+            this.trabalho.TabIndex = 3;
+            // 
+            // n2
+            // 
+            this.n2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n2.Location = new System.Drawing.Point(288, 235);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(396, 26);
+            this.n2.TabIndex = 4;
+            // 
+            // n1
+            // 
+            this.n1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n1.Location = new System.Drawing.Point(288, 89);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(396, 26);
+            this.n1.TabIndex = 5;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(432, 428);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(102, 37);
+            this.lblresultado.TabIndex = 6;
+            this.lblresultado.Text = "label4";
+            // 
+            // btcalc
+            // 
+            this.btcalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btcalc.Location = new System.Drawing.Point(416, 495);
+            this.btcalc.Name = "btcalc";
+            this.btcalc.Size = new System.Drawing.Size(131, 41);
+            this.btcalc.TabIndex = 7;
+            this.btcalc.Text = "CALCULAR";
+            this.btcalc.UseVisualStyleBackColor = true;
+            this.btcalc.Click += new System.EventHandler(this.Btcalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 572);
+            this.Controls.Add(this.btcalc);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.trabalho);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.TextBox trabalho;
+        private System.Windows.Forms.TextBox n2;
+        private System.Windows.Forms.TextBox n1;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.Button btcalc;
+    }
+}
+
---
+++ Aula4Media/Aula4Media/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula4Media
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btcalc_Click(object sender, EventArgs e)
+        {
+            double p1, p2, t, m;
+
+            p1 = Convert.ToDouble(n1.Text);
+            p2 = Convert.ToDouble(n2.Text);
+            t = Convert.ToDouble(trabalho.Text);
+
+
+        }
+    }
+}
