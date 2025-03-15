affc5db5ff9c1b2cde8bd4317c31dbb1aa4c9772
Alisson Evangelista <108416106+AlissonED@users.noreply.github.com>
2025-03-15 11:03:56 -0300

    Add files via upload

---
+++ Aula4Dados/Aula4Dados/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Aula4Dados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.btjogar = new System.Windows.Forms.Button();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.Color.White;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(174, 253);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(39, 33);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "...";
+            this.lbl1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(359, 336);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(25, 24);
+            this.lblresultado.TabIndex = 1;
+            this.lblresultado.Text = "...";
+            this.lblresultado.Click += new System.EventHandler(this.Lblresultado_Click);
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.BackColor = System.Drawing.Color.White;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(596, 253);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(39, 33);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "...";
+            // 
+            // btjogar
+            // 
+            this.btjogar.BackColor = System.Drawing.Color.Black;
+            this.btjogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btjogar.ForeColor = System.Drawing.Color.White;
+            this.btjogar.Location = new System.Drawing.Point(337, 363);
+            this.btjogar.Name = "btjogar";
+            this.btjogar.Size = new System.Drawing.Size(117, 50);
+            this.btjogar.TabIndex = 6;
+            this.btjogar.Text = "JOGAR";
+            this.btjogar.UseVisualStyleBackColor = false;
+            this.btjogar.Click += new System.EventHandler(this.Btjogar_Click);
+            // 
+            // pic1
+            // 
+            this.pic1.Location = new System.Drawing.Point(107, 84);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(155, 166);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic1.TabIndex = 4;
+            this.pic1.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.Location = new System.Drawing.Point(531, 84);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(155, 166);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic2.TabIndex = 5;
+            this.pic2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(810, 455);
+            this.Controls.Add(this.btjogar);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Button btjogar;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+    }
+}
+
---
+++ Aula4Dados/Aula4Dados/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula4Dados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btjogar_Click(object sender, EventArgs e)
+        {
+            Random sorteio = new Random();
+            int d1, d2;
+            d1 = sorteio.Next(1, 7);
+            d2 = sorteio.Next(1, 7);
+
+            lbl1.Text = Convert.ToString(d1);
+            lbl2.Text = d2.ToString();
+
+            /* caminho de imagem local :
+            pic1.Load("C:\\Users\\labs\\Documents\\dados\\" + d1 + ".png");
+            pic2.Load(@"C:\Users\labs\Documents\dados\" + d2 + ".png");*/
+
+            pic1.Load(Application.StartupPath +"\\dados\\" + d1 + ".png");
+            pic2.Load(Application.StartupPath + @"\dados\" + d2 + ".png");
+
+            if (d1==d2) {
+                lblresultado.Text = "Ganhou";
+                lblresultado.ForeColor = Color.Green;
+            } else {
+                lblresultado.Text = "Perdeu";
+                lblresultado.ForeColor = Color.Red;
+            }
+        }
+
+        private void Lblresultado_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
